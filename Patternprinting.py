c9f6ae0413a70377694382a7fc591d6f1065502e
Codewith-tatai <mrdeepsaha735101@gmail.com>
2024-12-31 10:31:27 +0530

    add new pattern

--- Patternprinting.py
+++ Patternprinting.py
@@ -10,3 +10,25 @@ def pattern1(N):
     # Call the pattern1 function with the argument n
 #pattern1(n)
 pattern1(5)
+
+
+#we have to print 
+
+"1
+"12
+"123
+"1234
+"12345"
+
+#our code is
+
+#def nForest(n: int) -> None:
+def pattern3(N):
+    for i in range(1,N+1):
+        for j in range (1,i+1):
+            print(j,end="")
+
+        print()
+        
+#pattern3(n)
+pattern3(5)
